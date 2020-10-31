07b75720a03f8bfaf5a0482f0300e959fee74924
makadown <maseflores79@gmail.com>
2020-10-31 16:05:37 -0600

    Usando localstorage

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+
 /** Dark or Light images */
 function imageMode(color) {
     image1.src = `img/undraw_proud_coder_${color}.svg`;
@@ -18,7 +19,7 @@ function darkMode() {
     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.replace('fa-sun','fa-moon');
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
     imageMode('dark');
 }
 
@@ -27,20 +28,37 @@ function lightMode() {
     nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.replace('fa-moon','fa-sun');
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
     imageMode('light');
+}
+
+function setMode(mode) {
+    if (mode === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    } else { toggleSwitch.checked = false; lightMode(); }
 }
 
 /** Switch Theme Dynamically */
 function switchTheme(event) {
-    if(event.target.checked) {
+    if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        darkMode();
+        localStorage.setItem('theme', 'dark');
+        setMode('dark');
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
-        lightMode();
+        localStorage.setItem('theme', 'light');
+        setMode('light');
     }
 }
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// check localstorage for theme
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    setMode(currentTheme);
+}
